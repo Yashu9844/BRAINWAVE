4e70f24060dcdddcc8031726a054c6e008af3ebd
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-07-07 19:34:32 +0530

    steup Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+
 export default function App() {
   return (
    <>
    <div className="">
-    
+       <ButtonGradient/>
    </div>
    </>
   )
